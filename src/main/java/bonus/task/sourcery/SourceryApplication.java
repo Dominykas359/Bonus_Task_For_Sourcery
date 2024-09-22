1004509315e642a4d6cb9a576fe12d1d0dd8c358
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-09-22 12:29:43 +0300

    initial commit

---
+++ src/main/java/bonus/task/sourcery/SourceryApplication.java
@@ -0,0 +1,13 @@
+package bonus.task.sourcery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bonus/task/sourcery/SourceryApplicationTests.java
@@ -0,0 +1,13 @@
+package bonus.task.sourcery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
